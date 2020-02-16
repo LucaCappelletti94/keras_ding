32a3674b1a8224b5ecd1fe38eb34c08fada0f154
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-02-16 11:29:25 +0100

    xtended testing

--- tests/test_jupyter_ding.py
+++ tests/test_jupyter_ding.py
@@ -5,5 +5,6 @@ from keras_ding.utils import InvisibleAudio
 def test_invisible_audio():
     InvisibleAudio(Ding()._path).play()
     InvisibleAudio(Ding()._path)._repr_html_()
+    InvisibleAudio(Ding("random")._path)._repr_html_()
     for sample in Ding.available_samples:
         InvisibleAudio(Ding(sample)._path).play()
